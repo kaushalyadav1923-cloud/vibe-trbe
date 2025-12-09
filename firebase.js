d7cec0be75b904c7c5b6f839d211fb5d4c642e34
kaushalyadav1923-cloud <kaushalyadav1923@gmail.com>
2025-12-09 10:54:14 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+
+// 👉 TERA FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCpr9MXotr-M7noA7G28irleItKUELCY8Q",
+  authDomain: "vibe-tribe-d5138.firebaseapp.com",
+  projectId: "vibe-tribe-d5138",
+  storageBucket: "vibe-tribe-d5138.firebasestorage.app",
+  messagingSenderId: "656217231234",
+  appId: "1:656217231234:web:e19e5e41836c4893c9899b",
+  measurementId: "G-BMJ9XQD4WD"
+};
+
+// 👉 Firebase Initialize
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
